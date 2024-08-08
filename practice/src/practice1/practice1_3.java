58e74c8415380581fca8a0b87adb102ded514b6e
Nami0115 <lifeihave156@gmail.com>
2024-08-08 22:11:51 +0900

    第1章の練習問題

---
+++ practice/src/practice1/practice1_3.java
@@ -0,0 +1,11 @@
+package practice1;
+
+public class practice1_3 {
+	public static void main(String[] args) {
+		boolean authenticity = true;
+		char word = '駆';
+		double decimal = 3.14;
+		long number = 314159265853979L;
+		String phrase = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+}
